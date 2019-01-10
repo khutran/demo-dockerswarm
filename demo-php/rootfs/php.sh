203ff089897eedb905bc55c3ddaf79db66fe8032
xuankhu <xuankhu1111@gmail.com>
2019-01-10 10:36:57 +0700

    update

--- demo-php/rootfs/php.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bashi
-# Make sure we're not confused by old, incompletely-shutdown httpd
-# context after restarting the container.  httpd won't start correctly
-# if it thinks it is already running.
-#rm -rf /tmp/supervisor*
-exec  php-fpm -F --pid /opt/bitnami/php/tmp/php-fpm.pid -y /opt/bitnami/php/etc/php-fpm.conf 
-#exec supervisord -c /etc/supervisord.conf
