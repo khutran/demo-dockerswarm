4ff6709090bced7373cc40da1e57761e8cc3cd3a
xuankhu <xuankhu1111@gmail.com>
2019-03-11 13:26:50 +0700

    update

--- product/nginx_php_node/scripts/backupdb.sh
+++ product/nginx_php_node/scripts/backupdb.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 DATE=$(date +%d-%m-%Y)
 BACKUP_DIR="/var/www/backup"
-BACKUP_DIR=$(printenv | grep BACKUP_DIR | cut -d "=" -f 2)
 MYSQL_USER=$(printenv | grep MYSQL_USER | cut -d "=" -f 2)
 MYSQL_PASSWORD=$(printenv | grep MYSQL_PASSWORD | cut -d "=" -f 2)
 MYSQL_ROOT_HOST=$(printenv | grep MYSQL_ROOT_HOST | cut -d "=" -f 2)
 
 
-mkdir -p ${BACKUP_DIR}/${DATE}
+if [ ! -d "$BACKUP_DIR" ]; then
+  mkdir -p "${BACKUP_DIR}/${DATE}"
+fi
+
 databases=`mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -h ${MYSQL_ROOT_HOST} -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema)"`
 
 for db in $databases
@@ -15,4 +17,7 @@ do
 echo $db
 mysqldump --force --opt --user=${MYSQL_USER} -p${MYSQL_PASSWORD} -h ${MYSQL_ROOT_HOST} --databases ${db} | gzip > "${BACKUP_DIR}/${DATE}/${db}.sql.gz"
 done
-find ${BACKUP_DIR}/* -mtime +10 -exec rm {} \;
+
+if [  -d "$BACKUP_DIR" ]; then
+    find ${BACKUP_DIR}/* -mtime +10 -exec rm {} \;
+fi
