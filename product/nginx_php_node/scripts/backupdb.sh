a40bd2b0346aec933f770a05e46283d31cd45d62
xuankhu <xuankhu1111@gmail.com>
2019-03-11 14:20:49 +0700

    update

--- product/nginx_php_node/scripts/backupdb.sh
+++ product/nginx_php_node/scripts/backupdb.sh
@@ -5,11 +5,7 @@ MYSQL_USER=$(printenv | grep MYSQL_USER | cut -d "=" -f 2)
 MYSQL_PASSWORD=$(printenv | grep MYSQL_PASSWORD | cut -d "=" -f 2)
 MYSQL_ROOT_HOST=$(printenv | grep MYSQL_ROOT_HOST | cut -d "=" -f 2)
 
-
-if [ ! -d "$BACKUP_DIR" ]; then
-  mkdir -p "${BACKUP_DIR}/${DATE}"
-fi
-
+mkdir -p "${BACKUP_DIR}/${DATE}"
 databases=`mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -h ${MYSQL_ROOT_HOST} -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema)"`
 
 for db in $databases
@@ -18,6 +14,4 @@ echo $db
 mysqldump --force --opt --user=${MYSQL_USER} -p${MYSQL_PASSWORD} -h ${MYSQL_ROOT_HOST} --databases ${db} | gzip > "${BACKUP_DIR}/${DATE}/${db}.sql.gz"
 done
 
-if [  -d "$BACKUP_DIR" ]; then
-    find ${BACKUP_DIR}/* -mtime +10 -exec rm {} \;
-fi
+find ${BACKUP_DIR}/* -mtime +10 -exec rm {} \;
