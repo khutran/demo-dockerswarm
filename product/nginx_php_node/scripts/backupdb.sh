ede6184e1ce15b3da1d4605dba4baec8aac460ad
xuankhu <xuankhu1111@gmail.com>
2019-03-11 10:53:54 +0700

    update

--- product/nginx_php_node/scripts/backupdb.sh
+++ product/nginx_php_node/scripts/backupdb.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
-
-# Add your backup dir location, password, mysql location and mysqldump location
 DATE=$(date +%d-%m-%Y)
 BACKUP_DIR="/var/www/backup"
-MYSQL_USER="root"
-MYSQL_PASSWORD="Quenanhdi123s"
-MYSQL=/u01/mysql/bin/mysql
-MYSQLDUMP=/u01/mysql/bin/mysqldump
+BACKUP_DIR=$(printenv | grep BACKUP_DIR | cut -d "=" -f 2)
+MYSQL_USER=$(printenv | grep MYSQL_USER | cut -d "=" -f 2)
+MYSQL_PASSWORD=$(printenv | grep MYSQL_PASSWORD | cut -d "=" -f 2)
 
-# To create a new directory into backup directory location
-mkdir -p $BACKUP_DIR/$DATE
 
-# get a list of databases
-databases=`$MYSQL -u$MYSQL_USER -p$MYSQL_PASSWORD -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema)"`
+mkdir -p ${BACKUP_DIR}/${DATE}
+databases=`mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -h ${MYSQL_ROOT_HOST} -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema)"`
 
-# dump each database in separate name
-for db in $databases; do
+for db in $databases
+do
 echo $db
-$MYSQLDUMP --force --opt --user=$MYSQL_USER -p$MYSQL_PASSWORD --databases $db | gzip > "$BACKUP_DIR/$DATE/$db.sql.gz"
+mysqldump --force --opt --user=${MYSQL_USER} -p${MYSQL_PASSWORD} -h ${MYSQL_ROOT_HOST} --databases ${db} | gzip > "${BACKUP_DIR}/${DATE}/${db}.sql.gz"
 done
-
-# Delete files older than 10 days
-find $BACKUP_DIR/* -mtime +10 -exec rm {} \;
+find ${BACKUP_DIR}/* -mtime +10 -exec rm {} \;
